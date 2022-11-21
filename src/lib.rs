7fcf8d389393982a53a309e8e37025e23aac2f06
LokiSharp <loki.sharp@gmail.com>
2022-11-22 00:40:46 +0800

    feat: 实现 CPU 异常

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
@@ -24,10 +26,10 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// Entry point for `cargo xtest`
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -53,3 +55,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         port.write(exit_code as u32);
     }
 }
+
+pub fn init() {
+    interrupts::init_idt();
+}
+
